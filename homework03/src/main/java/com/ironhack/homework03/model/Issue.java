ad1ea485d3d013af2d801b133d4d59ed34f66db9
Fernando <fernandobalsecasanchez@gmail.com>
2025-03-03 12:25:00 +0100

    model classes and new branch

--- homework03/src/main/java/com/ironhack/homework03/model/Issue.java
+++ homework03/src/main/java/com/ironhack/homework03/model/Issue.java
@@ -1,4 +1,83 @@
 package com.ironhack.homework03.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer issueId;
+    private LocalDate issueDate;
+    private LocalDate returnDate;
+
+    @OneToOne
+    @JoinColumn(name = "usn")
+    private Student issueStudent;
+
+    @OneToOne
+    @JoinColumn(name = "isbn")
+    private Book issueBook;
+
+    public Issue() {
+    }
+
+    public Issue(LocalDate issueDate, LocalDate returnDate, Student issueStudent, Book issueBook) {
+        this.issueDate = issueDate;
+        this.returnDate = returnDate;
+        this.issueStudent = issueStudent;
+        this.issueBook = issueBook;
+    }
+
+    public Integer getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(Integer issueId) {
+        this.issueId = issueId;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Student getIssueStudent() {
+        return issueStudent;
+    }
+
+    public void setIssueStudent(Student issueStudent) {
+        this.issueStudent = issueStudent;
+    }
+
+    public Book getIssueBook() {
+        return issueBook;
+    }
+
+    public void setIssueBook(Book issueBook) {
+        this.issueBook = issueBook;
+    }
+
+    @Override
+    public String toString() {
+        return "Issue{" +
+                "issueId=" + issueId +
+                ", issueDate=" + issueDate +
+                ", returnDate=" + returnDate +
+                ", issueStudent=" + issueStudent +
+                ", issueBook=" + issueBook +
+                '}';
+    }
 }
--- homework03/src/main/java/com/ironhack/homework03/model/Student.java
+++ homework03/src/main/java/com/ironhack/homework03/model/Student.java
@@ -1,4 +1,45 @@
 package com.ironhack.homework03.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Student {
+    @Id
+    private String usn;
+    private String name;
+
+    @OneToOne(mappedBy = "issueStudent", cascade = CascadeType.ALL)
+    private Issue issue;
+
+    public Student() {
+    }
+
+    public Student(String usn, String name) {
+        this.usn = usn;
+        this.name = name;
+    }
+
+    public String getUsn() {
+        return usn;
+    }
+
+    public void setUsn(String usn) {
+        this.usn = usn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "usn='" + usn + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }

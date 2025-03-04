07dc6b6d34e30b961239ade20af4f698e5d2d393
unknown <gymest.01@gmail.com>
2025-03-04 10:37:38 +0100

    command

--- homework03/src/main/java/com/ironhack/homework03/model/Book.java
+++ homework03/src/main/java/com/ironhack/homework03/model/Book.java
@@ -1,4 +1,33 @@
 package com.ironhack.homework03.model;
 
-public class Book {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+    @Entity
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Book {
+        @Id
+        private String isbn;
+        private String title;
+        private String category;
+        private int quantity;
+
+        public Book(String isbn, String title, String category, int quantity) {
+            this.isbn = isbn;
+            this.title = title;
+            this.category = category;
+            this.quantity = quantity;
+        }
+
+        //    @OneToOne(mappedBy = "authorbook")
+//    private Book book;
+//
+//    @OneToOne(mappedBy = "issuebook")
+//    private Issue issue;
 }

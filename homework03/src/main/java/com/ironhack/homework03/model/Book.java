0f1a8cec503e548fe1bde5b587fdf467fef6c930
unknown <gymest.01@gmail.com>
2025-03-04 11:22:19 +0100

    Add the test structure in BookRepositoryTest. There are nothing test.

--- homework03/src/main/java/com/ironhack/homework03/model/Book.java
+++ homework03/src/main/java/com/ironhack/homework03/model/Book.java
@@ -7,20 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Book {
-    @Id
-    private String isbn;
-    private String title;
-    private String category;
-    private int quantity;
-
-//    @OneToOne(mappedBy = "authorbook")
-=======
     @Entity
     @Data
     @NoArgsConstructor
@@ -32,15 +18,11 @@ public class Book {
         private String category;
         private int quantity;
 
-        public Book(String isbn, String title, String category, int quantity) {
-            this.isbn = isbn;
-            this.title = title;
-            this.category = category;
-            this.quantity = quantity;
-        }
+
+
 
         //    @OneToOne(mappedBy = "authorbook")
->>>>>>> maindev03
+
 //    private Book book;
 //
 //    @OneToOne(mappedBy = "issuebook")

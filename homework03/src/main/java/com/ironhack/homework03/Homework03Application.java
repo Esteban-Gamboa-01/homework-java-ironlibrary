9a52a7670017fdb64716c5f001a4243d5fd8ca67
Fernando <fernandobalsecasanchez@gmail.com>
2025-03-05 11:36:08 +0100

    Menu CommandLineRunner

--- homework03/src/main/java/com/ironhack/homework03/Homework03Application.java
+++ homework03/src/main/java/com/ironhack/homework03/Homework03Application.java
@@ -1,13 +1,22 @@
 package com.ironhack.homework03;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Homework03Application {
+public class Homework03Application implements CommandLineRunner {
+
+	@Autowired
+	Menu menu;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Homework03Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		menu.showMenu();
+	}
 }
